6eda016545a0192a7a267b9b9a5be44019b05433
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-07-09 18:50:22 -0700

    Implemented the Update method

--- trainSchedule.js
+++ trainSchedule.js
@@ -11,71 +11,90 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 let trainAddObj = {};
+let trainUpdateObj = {};
+let snapReadonceObjKeys=[];
+let snapReadonceObjValues=[];
 let databaseURL = firebase.database();
-let validInputs = true;
+let validInputsAdd = true;
+let validInputsUpdate = true;
 
 $(document).ready(function (eventReadyObj) {
-    console.log(eventReadyObj);
-    // Method triggers once at page load and runs every time a new child is added 
-    databaseURL.ref("/trainschedules").on('child_added', function (snapObj) {
-        console.log(snapObj.key);
-        console.log(snapObj.val());
-        const fireObject = snapObj.val();
 
-        let tFrequency = fireObject.frequency;
-        let firstTime = fireObject.trainTime;
-        console.log(firstTime);
+    databaseURL.ref("/trainschedules").on('child_changed', function (snapChangedObj) {
+        console.log(snapChangedObj.key);
+        console.log(snapChangedObj.val());
+    });
+
+    databaseURL.ref("/trainschedules").on('child_removed', function (snapRemovedObj) {
+        console.log("Inside the Child Removed Function");
+        console.log(snapRemovedObj.key);
+        console.log(snapRemovedObj.val());
+        //Remove the element from DOM
+        $("tr").remove("#" + snapRemovedObj.key);
+    });
+
+    // databaseURL.ref("trainschedules").once('value', function (snapReadonceObj) {
+    //     snapReadonceObjKeys = Object.keys(snapReadonceObj.val());
+    //     snapReadonceObjValues = Object.values(snapReadonceObj.val());
+    //     console.log(snapReadonceObjKeys);
+    //     console.log(snapReadonceObjValues);
+    // });
+
+    // Method triggers once at page load and runs every time a new child is added 
+    databaseURL.ref("/trainschedules").on('child_added', function(snapChildAddedObj) {
+        console.log(snapChildAddedObj.key);
+        console.log(snapChildAddedObj.val());
+
+        snapReadonceObjKeys.push(snapChildAddedObj.key);
+        snapReadonceObjValues.push(snapChildAddedObj.val());
+        console.log(snapReadonceObjKeys);
+        console.log(snapReadonceObjValues);
+
+        const ChildAddedObj = snapChildAddedObj.val();
+        let tFrequency = ChildAddedObj.frequency;
+        let firstTime = ChildAddedObj.trainTime;
+
         // First Time (pushed back 1 year to make sure it comes before current time)
         let firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-        console.log(firstTimeConverted);
 
         // Current Time
-        let currentTime = moment().format("HH:mm");
-        console.log("CURRENT TIME: " + currentTime);
+        let currentTime = moment().format("YYYY dddd,MMMM,DD HH:mm A");
+        console.log("DATE & TIME: " + currentTime);
 
         // Difference between the times
         let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
 
         // Time apart (remainder)
         let tRemainder = diffTime % tFrequency;
-        console.log(tRemainder);
 
         // Minute Until Train
         let tMinutesTillTrain = tFrequency - tRemainder;
-        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
         // Next Train
-        let nextTrain = moment().add(tMinutesTillTrain, "minutes");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+        let nextTrain = moment().add(tMinutesTillTrain, "minutes").format("HH:mm A");
 
-        console.log(moment().add(1200, "minutes").format("HH:mm"));
-
-        const tRow = $("<tr>").attr({ 'id': snapObj.key }).append(
-            $("<td>").text(fireObject.trainName),
-            $("<td>").text(fireObject.destination),
-            $("<td>").text(fireObject.frequency),
-            $("<td>").text(moment(nextTrain).format("hh:mm")),
-            $("<td>").text(tMinutesTillTrain).append($("<button>").attr({ 'id': snapObj.key, class: "tRowButton" }).css({ float: "right" }).text("X"))
+        const tRow = $("<tr>").attr({ 'id': snapChildAddedObj.key }).append(
+            $("<td>").text(ChildAddedObj.trainName),
+            $("<td>").text(ChildAddedObj.destination),
+            $("<td>").text(ChildAddedObj.frequency),
+            $("<td>").text(nextTrain),
+            $("<td>").text(tMinutesTillTrain).append($("<button>").attr({ 'id': snapChildAddedObj.key, class: "tRowButton" }).css({ float: "right" }).text("X"))
         );
-
+        
         $("#trainSchedule > tbody").append(tRow);
 
     });
 
-    $(document).on('click', ".tRowButton", function (eventObj) {
-        console.log(eventObj);
+    $(document).on('click', ".tRowButton", function (eventDeleteObj) {
+        eventDeleteObj.preventDefault();
         console.log($(this).attr('id'));
         //Call this method to remove data from firebase
         databaseURL.ref("/trainschedules").child($(this).attr('id')).remove();
-
-        //Remove the element from DOM
-        $("tr").remove("#" + $(this).attr('id'));
-
     });
 
-    $("#trainAddButton").click(function (eventObj) {
-        console.log(eventObj);
+    $("#trainAddButton").click(function (eventAddObj) {
+        eventAddObj.preventDefault();
+        trainAddObj = {};
         trainAddObj.trainName = $("#trainName").val().trim();
         trainAddObj.destination = $("#destination").val().trim();
         trainAddObj.trainTime = $("#trainTime").val().trim();
@@ -83,17 +102,13 @@ $(document).ready(function (eventReadyObj) {
         console.log(trainAddObj);
 
         $("form :input").each(function (index, element) {
-            console.log(index);
-            console.log(element);
-            console.log($(this));
-            console.log($(this).attr('id'));
-            console.log($(this).val().trim());
+            //  console.log("index: " + index,element,"attribute:" + $(this).attr('id'),"Value: " + $(this).val().trim());
             if ($(this).val().trim() == "") {
                 //Select element by ID
                 $("#" + $(this).attr('id')).focus();
                 alert("Enter the data for the field: " + $(this).attr('id'));
-                validInputs = false;
-                return validInputs;
+                validInputsAdd = false;
+                return validInputsAdd;
             }
         });
 
@@ -106,13 +121,89 @@ $(document).ready(function (eventReadyObj) {
             $("#trainTime").val("");
             $("#frequency").val("");
         }
+    });
+
+    $("#trainUpdateButton").click(function (eventUpdateObj) {
+        eventUpdateObj.preventDefault();
+        let foundChild = false;
+        let ChildTrainNameKey = "";
+
+        trainUpdateObj = {};
+        trainUpdateObj.trainName = $("#trainName").val().trim();
+
+        // if (trainUpdateObj.trainName == "") {
+        //     validInputsUpdate = false;
+        //     alert("Enter the data for the field: " + $("#trainName").attr('id'));
+        //     $("#trainName").focus();
+        //     console.log(validInputsUpdate);
+        // }
+        // else if (trainUpdateObj.trainName != "") {
+        //     validInputsUpdate = true;
+        // }
+
+        $("form :input").each(function (index, element) {
+            console.log("index: " + index, element, "attribute:" + $(this).attr('id'), "Value: " + $(this).val().trim());
+            if ($(this).val().trim() != "") {
+                //Select element by ID
+                const element = $(this).attr('id');
+                console.log(element);
+                trainUpdateObj[element] = $(this).val().trim();
+                console.log(trainUpdateObj);
+            }
+        });
+
+        if (validInputsUpdate) {
+            databaseURL.ref("/trainschedules").once('value')
+                .then(function (snapshotObj) {
+                    snapshotObj.forEach(function (childSnapObj) {
+                        console.log(childSnapObj.val());
+                        console.log(childSnapObj.key);
+                        let trainUpdateObjlocal = {};
+                        trainUpdateObjlocal = childSnapObj.val();
+                        if (trainUpdateObjlocal.trainName == $("#trainName").val().trim()) {
+                            foundChild = true;
+                            ChildTrainNameKey = childSnapObj.key;
+                            console.log(foundChild, ChildTrainNameKey);
+                        }
+                    });
+
+                    if (foundChild) {
+                        trainUpdateObjlocal = {};
+                        databaseURL.ref("/trainschedules").child(ChildTrainNameKey).once("value")
+                            .then(function (snapChildObj) {
+                                console.log(snapChildObj.key);
+                                console.log(snapChildObj.val());
+                            });
+
+                        if (Object.keys(trainUpdateObj).length == 1) {
+                            alert("Enter the values to the input fields");
+                        }
+                        else {
+
+                            console.log(trainUpdateObj);
+                            let result = databaseURL.ref("/trainschedules").child(ChildTrainNameKey).update(trainUpdateObj);
+
+                            console.log(result);
+                            //Clear the text box values 
+                            $("#trainName").val("");
+                            $("#destination").val("");
+                            $("#trainTime").val("");
+                            $("#frequency").val("");
+                            window.location.reload();
+                        }
+
+                    }
+                    // else {
+                    //     alert("Enter valid Train Name to Update!!");
+                    // }
+
+                })
+                .catch(function (errorUpdateObj) {
+                    console.log(errorUpdateObj);
+                });
+        }
 
     });
 
-    console.log($(":input").length);
-
-    console.log($("form > *").length);
-    console.log($("form :input"));
-    console.log($('div[class="form-group px-md-3"]'));
 });
 
